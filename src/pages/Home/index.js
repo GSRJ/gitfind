ecf2eae671345c5069a3a5dc1f34338d5d046f2e
GSRJ <gerson.contato@outlook.com>
2023-09-01 00:10:14 -0300

    feat: estrutura da Home, imagem de fundo, input e estilização

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,27 @@
+import background from "../../assets/background.svg";
 import { Header } from "../../components/Header";
+import "./style.css";
 
 export const App = () => {
   return (
     <div className='App'>
       <Header />
+      <div className='content'>
+        <img
+          src={background}
+          className='background'
+          alt='background app'
+        />
+        <div className='info'>
+          <div className='inputBox'>
+            <input
+              name='username'
+              placeholder='@username'
+            />
+            <button type='submit'>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
