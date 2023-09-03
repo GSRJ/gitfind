6f68fecc7a62bb02082c947ab8c57843dd48d2d4
GSRJ <gerson.contato@outlook.com>
2023-09-02 23:47:45 -0300

    feat: caixa de informações do perfil com imagem.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,3 @@
-import background from "../../assets/background.svg";
 import { Header } from "../../components/Header";
 import "./style.css";
 
@@ -6,19 +5,32 @@ export const App = () => {
   return (
     <div className='App'>
       <Header />
-      <div className='content'>
-        <img
-          src={background}
-          className='background'
-          alt='background app'
-        />
-        <div className='info'>
-          <div className='inputBox'>
-            <input
-              name='username'
-              placeholder='@username'
-            />
-            <button type='submit'>Buscar</button>
+      <div className='container'>
+        <div className='inputBox'>
+          <input
+            name='username'
+            placeholder='@username'
+          />
+          <button type='submit'>Buscar</button>
+        </div>
+        <div className='content'>
+          <div className='pageContent'>
+            <div className='dataBox'>
+              <img
+                src='https://avatars.githubusercontent.com/u/108104426?v=4'
+                className='profileImg'
+                alt='profile'
+              />
+              <div className='profileInfo'>
+                <h3>Gerson Sousa Rocha Junior</h3>
+                <span>@username</span>
+                <p>
+                  Desciption duigdfuefguef ofheihfi8efvbdjvdv bfi8wefubcvjhbvb
+                  fniefuiegbufbfjhf qwofjiehfjvcvb
+                </p>
+              </div>
+            </div>
+            <hr />
           </div>
         </div>
       </div>
